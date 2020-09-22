ef8f9c6d3f9686522bdbd6f79d492e4f997beefe
unknown <seuemail@email.com>
2020-09-22 16:30:46 -0300

    Incluindo arquivos faltantes do commit anterior

--- Empregado.java
+++ Empregado.java
@@ -21,15 +21,21 @@ public class Empregado{
     }
     }
     public String getNome (){
-    return this.nome;
+        return this.nome;
     }
-    public void setNome (String nome){
-    this.nome = nome;
+    public void setNome (final String nome){
+        this.nome = nome;
     }
     public int getIdade (){
-    return this.idade;
+        return this.idade;
     }
     public void setIdade (int idade){
-    this.idade = idade;
+        this.idade = idade;
     }
+    public void setTipo (int tipo){
+        this.tipo = tipo;
+        }
+        public int tipo (){
+        return this.tipo;
+        }
     }
