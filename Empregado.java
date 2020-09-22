742d9a5bbb8532d84f7d4c3d5b591c961ef425f1
unknown <adv.deboramaria@gmail.com>
2020-09-22 02:35:09 -0300

    Commit inicial. Classe Empregado.java criada

---
+++ Empregado.java
@@ -0,0 +1,18 @@
+public class Empregado{
+    private String nome;
+    private int idade;
+    public String getNome (){
+    return this.nome;
+    }
+    public void setNome (final String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(final int idade) {
+    this.idade = idade;
+    }
+    }
